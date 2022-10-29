89d0282038485634a3591b0d5df8b75b24cfbb2a
Ezequiel Acosta <ezekiel.acosta.94@gmail.com>
2022-10-29 11:58:32 -0300

    Se crea el proyecto de BackEnd con Spring

---
+++ src/main/java/com/portfolio/jea/JeaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.jea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/jea/JeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.jea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
