15938fd8383af35f3fbcd07f6079ebd858551b85
Ezequiel Acosta <ezekiel.acosta.94@gmail.com>
2022-10-29 16:49:00 -0300

    Codificación en NetBeans del BackEnd separado en capas

---
+++ src/main/java/com/portfolio/jea/Repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.portfolio.jea.Repository;
+
+import com.portfolio.jea.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona, Long> {
+    
+}
