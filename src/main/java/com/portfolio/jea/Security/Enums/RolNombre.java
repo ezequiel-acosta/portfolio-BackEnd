15c2031499db41456a9b6a73e4d8f01d62997bf6
Ezequiel Acosta <ezekiel.acosta.94@gmail.com>
2022-10-30 00:44:07 -0300

    Se implementa Login con JWT

---
+++ src/main/java/com/portfolio/jea/Security/Enums/RolNombre.java
@@ -0,0 +1,5 @@
+package com.portfolio.jea.Security.Enums;
+
+public enum RolNombre {
+    ROLE_ADMIN, ROLE_USER
+}
---
+++ src/main/java/com/portfolio/jea/Security/Repository/iRolRepository.java
@@ -0,0 +1,12 @@
+package com.portfolio.jea.Security.Repository;
+
+import com.portfolio.jea.Security.Entity.Rol;
+import com.portfolio.jea.Security.Enums.RolNombre;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface iRolRepository extends JpaRepository<Rol, Integer> {
+    Optional<Rol> findByRolNombre(RolNombre rolNombre);
+}
