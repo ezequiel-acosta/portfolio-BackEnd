93c02cd95c46f2e8a7b9f6a201f1b5feb414a06d
Ezequiel Acosta <ezekiel.acosta.94@gmail.com>
2022-10-31 18:27:16 -0300

    Se actualiza la URL del FrontEnd

--- src/main/java/com/portfolio/jea/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/jea/Controller/CExperiencia.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("explab")
-@CrossOrigin(origins = {"https://jeafrontend.web.app", "http://localhost:4200"})
+@CrossOrigin(origins = {"https://frontendjea.web.app/", "http://localhost:4200"})
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
--- src/main/java/com/portfolio/jea/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/jea/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://jeafrontend.web.app", "http://localhost:4200"})
+@CrossOrigin(origins = {"https://frontendjea.web.app/", "http://localhost:4200"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
