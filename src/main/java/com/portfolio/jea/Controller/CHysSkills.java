c7977e5017ade6d67542caa86f02f23dd4d2ace9
Ezequiel Acosta <ezekiel.acosta.94@gmail.com>
2022-10-31 13:36:50 -0300

    Arreglos minimos

--- src/main/java/com/portfolio/jea/Controller/CHysSkills.java
+++ src/main/java/com/portfolio/jea/Controller/CHysSkills.java
@@ -20,8 +20,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http:localhost:4200")
-@RequestMapping("/hys")
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/skill")
 public class CHysSkills {
 
     @Autowired
